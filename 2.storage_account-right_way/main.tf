07fa7fbc2b753eab8989b366b3979b4941c7b379
Charis <Charisnnadi15@gmail.com>
2025-06-21 01:33:04 +0100

    stored the statefile remotely

---
+++ 2.storage_account-right_way/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "replace-with-your-storage-account-name" # Replace with your Azure Storage Account name after creating it
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+
+  required_version = ">= 1.9.0"
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  subscription_id = "replace-with-your-subscription-id" # Replace with your Azure subscription ID
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resourcesing"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccount86775456" # Must be globally unique
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location #implicit dependency
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
