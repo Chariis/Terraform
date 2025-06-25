a26e34c2f92e59a3b0174261ed3be790c429d9e2
Charis <Charisnnadi15@gmail.com>
2025-06-25 02:56:53 +0100

    count vs for_each

---
+++ 6.countVSfor_each/variable.tf
@@ -0,0 +1,29 @@
+variable "environment" {
+  type        = string
+  description = "the env type"
+  default     = "staging"
+
+}
+
+variable "allowed_locations" {
+  type        = list(string)
+  description = "list of allowed locations"
+  default     = ["West Europe", "North Europe", "East US"]
+
+}
+
+variable "resource_tags" {
+  type        = map(string)
+  description = "tags to apply to the resources"
+  default = {
+    "environment" = "staging"
+    "managed_by"  = "terraform"
+    "department"  = "devops"
+  }
+
+}
+
+variable "storage_account_name" {
+  type    = set(string)
+  default = ["goziestorage1", "goziestorage2"]
+}
